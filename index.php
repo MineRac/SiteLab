50b80bfd7d4b5a6935457933c608190dfd740bcd
MineRac <75296368+MineRac@users.noreply.github.com>
2023-11-17 03:59:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="http://45.152.114.177/php/static/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Site</title>
 </head>
 <body>  
 <div class="buttons">
-    <a href="http://45.152.114.177/php/Gallery.php">Галерея</a>
+    <a href="Gallery.php">Галерея</a>
     <a href="https://t.me/danya_maloy">Телеграм</a>
     <a href="mailto:minerec5@gmail.com">Почта</a>
-    <a href="http://45.152.114.177/php/about.php">Обо мне</a>
-    <a href="http://45.152.114.177/php/login.php">
+    <a href="about.php">Обо мне</a>
+    <a href="login.php">
         <?php
         $dbhelper = new DBHelper();
         if (isset($_COOKIE['token'])) {
@@ -31,7 +31,7 @@
     <div class="burger-menu">
         <div class="block">
             <span class="title">Ссылки</span>
-            <a href="http://45.152.114.177/php/login.php">
+            <a href="login.php">
                 <?php
                 $dbhelper = new DBHelper();
                 if (isset($_COOKIE['token'])) {
@@ -46,15 +46,15 @@
         </div>
         <div class="block">
             <span class="title">Страницы</span>
-            <a style="width: fit-content" href="http://45.152.114.177/php/index.php">Главная</a>
-            <a style="width: fit-content" href="http://45.152.114.177/php/page2.php">Страница 2</a>
-            <a style="width: fit-content" href="http://45.152.114.177/php/page3.php">Страница 3</a>
-            <a style="width: fit-content" href="http://45.152.114.177/php/page4.php">Страница 4</a>
-            <a style="width: fit-content" href="http://45.152.114.177/php/page5.php">Страница 5</a>
+            <a style="width: fit-content" href="index.php">Главная</a>
+            <a style="width: fit-content" href="page2.php">Страница 2</a>
+            <a style="width: fit-content" href="page3.php">Страница 3</a>
+            <a style="width: fit-content" href="page4.php">Страница 4</a>
+            <a style="width: fit-content" href="page5.php">Страница 5</a>
         </div>
     </div>
     <div class="burger-icon" onclick="show_burger()">
-        <img src="http://45.152.114.177/php/static/burger.svg" >
+        <img src="burger.svg" >
     </div>
     <div class="block">
         <div class="avatar">
